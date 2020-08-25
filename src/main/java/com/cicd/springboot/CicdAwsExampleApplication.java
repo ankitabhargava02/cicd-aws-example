5dacdc31c21708ad1992621dada0333b6888aeca
ankita bhargava <ankitabhargava02@gmail.com>
2020-08-26 01:09:05 +0530

    Third Comit

--- src/main/java/com/cicd/springboot/CicdAwsExampleApplication.java
+++ src/main/java/com/cicd/springboot/CicdAwsExampleApplication.java
@@ -2,12 +2,19 @@ package com.cicd.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdAwsExampleApplication {
+public class CicdAwsExampleApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdAwsExampleApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CicdAwsExampleApplication.class);
 	}
 
 }
