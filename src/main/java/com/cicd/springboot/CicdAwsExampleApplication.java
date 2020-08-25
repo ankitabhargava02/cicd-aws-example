2343735c0d3cd0c3e8c15b30d955ce0ce110e789
ankita bhargava <ankitabhargava02@gmail.com>
2020-08-25 23:32:43 +0530

    Initial Comit

---
+++ src/main/java/com/cicd/springboot/CicdAwsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAwsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAwsExampleApplication.class, args);
+	}
+
+}
